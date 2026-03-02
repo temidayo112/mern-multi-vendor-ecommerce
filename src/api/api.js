9e42ad9f2576a9c7e673017f9fadb2b5f214949f
Admin <teedeedesigns@yahoo.com>
2026-03-02 09:53:36 +0100

    cook

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const local = 'http://localhost:5000'
-const production = 'https://backend-apiall-multi-vendor-ecommerce-uz1t.onrender.com'
+const production = 'https://backend-mern-multi-vendor-ecommerce-3hpw.onrender.com'
 
 let api_url = ''
 let mode = 'pro'
